922c51b8aa8695713e4945873589329e7645dc99
Timothy <timkhumpan@gmail.com>
2023-10-07 09:53:47 -0400

    update App

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,9 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./output.css";
 
 function App() {
   return (
+    // <div className="App">
     <div className="App">
+      <form
+        className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+        action="http://localhost:8080/login"
+        method="post"
+      >
+        <div className="mb-4 flex flex-col gap-6">
+          <p className="underline">Enter Login Info for LinkedIn:</p>
+          <p>
+            <input type="text" name="user" placeholder="username" />
+          </p>
+          <p>
+            <input type="text" name="pass" placeholder="password" />
+          </p>
+          <p>
+            <input type="submit" value="submit" />
+          </p>
+        </div>
+      </form>
       <p>THIS THE APP</p>
     </div>
   );
